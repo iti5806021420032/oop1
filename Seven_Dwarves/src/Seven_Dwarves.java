0cf8968105989d0ec2f4d4410107afc0b485743b
PC <PC@PC-PC>
2016-02-11 00:41:48 +0700

    น.ส.ฉัตรฑริกา ไชยวัน 5806021420032

---
+++ Seven_Dwarves/src/Seven_Dwarves.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+public class Seven_Dwarves {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int person1,person2,person3,person4,person5,person6,person7,person8,person9;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter person 1 : ");
+		person1 = scan.nextInt();
+		System.out.print("Enter person 2 : ");
+		person2 = scan.nextInt();
+		System.out.print("Enter person 3 : ");
+		person3 = scan.nextInt();
+		System.out.print("Enter person 4 : ");
+		person4 = scan.nextInt();
+		System.out.print("Enter person 5 : ");
+		person5 = scan.nextInt();
+		System.out.print("Enter person 6 : ");
+		person6 = scan.nextInt();
+		System.out.print("Enter person 7 : ");
+		person7 = scan.nextInt();
+		System.out.print("Enter person 8 : ");
+		person8 = scan.nextInt();
+		System.out.print("Enter person 9 : ");
+		person9 = scan.nextInt();
+		System.out.println();
+		if(person3+person4+person5+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person1 & person2");
+		if(person2+person4+person5+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person1 & person3");
+		if(person3+person2+person5+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person1 & person4");
+		if(person3+person4+person2+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person1 & person5");
+		if(person3+person4+person5+person2+person7+person8+person9==100)
+		System.out.print("Fake person is person1 & person6");
+		if(person3+person4+person5+person6+person2+person8+person9==100)
+		System.out.print("Fake person is person1 & person7");
+		if(person3+person4+person5+person6+person7+person2+person9==100)
+		System.out.print("Fake person is person1 & person8");
+		if(person3+person4+person5+person6+person7+person8+person2==100)
+		System.out.print("Fake person is person1 & person9");
+		if(person1+person4+person5+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person2 & person3");
+		if(person3+person1+person5+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person2 & person4");
+		if(person3+person4+person1+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person2 & person5");
+		if(person3+person4+person5+person1+person7+person8+person9==100)
+		System.out.print("Fake person is person2 & person6");
+		if(person3+person4+person5+person6+person1+person8+person9==100)
+		System.out.print("Fake person is person & person7");
+		if(person3+person4+person5+person6+person7+person1+person9==100)
+		System.out.print("Fake person is person2 & person8");
+		if(person3+person4+person5+person6+person7+person8+person1==100)
+		System.out.print("Fake person is person2 & person9");
+		if(person1+person2+person5+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person3 & person4");
+		if(person1+person4+person2+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person3 & person5");
+		if(person1+person4+person5+person2+person7+person8+person9==100)
+		System.out.print("Fake person is person3 & person6");
+		if(person1+person4+person5+person6+person2+person8+person9==100)
+		System.out.print("Fake person is person3 & person7");
+		if(person1+person4+person5+person6+person7+person2+person9==100)
+		System.out.print("Fake person is person3 & person8");
+		if(person1+person4+person5+person6+person7+person8+person2==100)
+		System.out.print("Fake person is person3 & person9");
+		if(person3+person1+person2+person6+person7+person8+person9==100)
+		System.out.print("Fake person is person4 & person5");
+		if(person3+person1+person5+person2+person7+person8+person9==100)
+		System.out.print("Fake person is person4 & person6");
+		if(person3+person1+person5+person6+person2+person8+person9==100)
+		System.out.print("Fake person is person4 & person7");
+		if(person3+person1+person5+person6+person7+person2+person9==100)
+		System.out.print("Fake person is person4 & person8");
+		if(person3+person1+person5+person6+person7+person8+person2==100)
+		System.out.print("Fake person is person4 & person9");
+		if(person3+person4+person1+person2+person7+person8+person9==100)
+		System.out.print("Fake person is person5 & person6");
+		if(person3+person4+person1+person6+person2+person8+person9==100)
+		System.out.print("Fake person is person5 & person7");
+		if(person3+person4+person1+person6+person7+person2+person9==100)
+		System.out.print("Fake person is person5 & person8");
+		if(person3+person4+person1+person6+person7+person8+person2==100)
+		System.out.print("Fake person is person5 & person9");
+		if(person3+person4+person5+person1+person2+person8+person9==100)
+		System.out.print("Fake person is person6 & person7");
+		if(person3+person4+person5+person1+person7+person2+person9==100)
+		System.out.print("Fake person is person6 & person8");
+		if(person3+person4+person5+person1+person7+person8+person2==100)
+		System.out.print("Fake person is person6 & person9");
+		if(person3+person4+person5+person6+person1+person2+person9==100)
+		System.out.print("Fake person is person7 & person8");
+		if(person3+person4+person5+person6+person1+person8+person2==100)
+		System.out.print("Fake person is person7 & person9");
+		if(person3+person4+person5+person6+person7+person1+person2==100)
+		System.out.print("Fake person is person8 & person9");
+	}
+
+}
